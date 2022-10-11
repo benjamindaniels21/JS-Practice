44f93aab4db300ee01009bc29e7dc0d52affab88
Ben Daniels <benjamindaniels21@gmail.com>
2022-10-11 17:49:10 -0400

    draw a random card challenge

--- test.js
+++ test.js
@@ -128,3 +128,17 @@
 // }
 
 // isPangram("Nymphs blit quick ve dwarf jo");
+
+const suits = ["spades", "hearts", "clubs", "diamonds"];
+const cardVal = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+const card = {};
+
+const randomCard = () => {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomCardVal = cardVal[Math.floor(Math.random() * cardVal.length)];
+  card.suit = randomSuit;
+  card.value = randomCardVal;
+  console.log(card);
+};
+
+randomCard();
